1ac16e4f9e92ad2613f267c23cd5c5562e37fd3a
Minter0817 <adf5858@naver.com>
2025-05-27 01:24:35 +0900

    Feat: 강의실 상세 조회 라우터 구현

---
+++ app/src/router/room/get_room.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from fastapi.responses import JSONResponse
+from fastapi.requests import Request as FastAPIRequest
+from app.model.Room import Room as RoomModel #SQLAlchemy 모델
+from app.schema.Room import Room as RoomSchema #Pydantic 모델
+from app.database import get_db
+
+app = APIRouter()
+
+@app.excption_handler(HTTPException)
+async def custom_http_exception_handeler(request: FastAPIRequest, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"error":exc.detail}
+    )
+
+@app.get("/api/rooms/{room_id}")
+async def get_room(room_id: str, db: Session = Depends(get_db)):
+    room = db.query(RoomModel).filter(RoomModel.room_id == room_id).first()
+    
+    if not room:
+        raise HTTPException(
+            status_code=404,
+            detail="Room not found"
+        )
+        
+    result = RoomSchema.from_orm(room)
+    return result
