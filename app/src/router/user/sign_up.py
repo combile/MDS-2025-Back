12a785317445c299aa12723eb2ac38cdaf049019
Lee minji <adf5858@naver.com>
2025-05-26 20:48:07 +0900

    Feat: 회원가입 라우터 구현

---
+++ app/src/router/user/sign_up.py
@@ -0,0 +1,57 @@
+from fastapi import APIRouter, Depends, HTTPException, Request, status
+from fastapi.responses import JSONResponse
+from fastapi.requests import Request as FastAPIRequest
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.model import User
+from app.schema.User import UserSignUp
+
+
+
+app = APIRouter()
+
+
+#MARK: - 커스텀 예외 핸들러
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request: FastAPIRequest, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"error": exc.detail}
+    )
+
+
+
+@app.post("/api/users/signup")
+async def sign_up(request : UserSignUp, db: Session=Depends(get_db)):
+    
+    if db.query(User).filter((User.id == request.id) | (User.student_id == request.student_id)).firts():
+        raise HTTPException(
+            status_code = 409,
+            detail="User already exists"
+        )
+        
+    #User(entity) 생성 : DB에 저장
+    #left: 컬럼 / right: pydantic 모델 필드
+    new_user = User(
+        student_id=request.student_id,
+        id=request.id,
+        password = request.pasword, #FIXME: 해싱처리 필요
+        department=request.department,
+        phone=request.phone_number,
+        email=request.email,
+        profile_photo=request.profile_photo
+    )
+    
+    #레코드 저장
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return {"message":"Sign up successful"}
+    
+    
+    
+    
+    
+    
+    
